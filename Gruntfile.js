83d82cda99b512dd74b905e27eec62de1f8afaa4
Paul Oaten <web@pca-uk.com>
2015-05-20 12:21:29 +0100

    Install Grunt & Sass

---
+++ Gruntfile.js
@@ -0,0 +1,47 @@
+module.exports = function(grunt) {
+	
+	grunt.initConfig({
+
+		pkg: grunt.file.readJSON('package.json'),
+
+		/* *
+		 * Sass Task
+		 */
+		sass: {
+			dev: {
+				options: {
+					style: 'expanded',
+					// sourcemap: 'none',
+				},
+				files: {
+					'css/style.css':'sass/style.scss'
+				}
+			},
+			dist: {
+				options: {
+					style: 'compressed',
+					// sourcemap: 'none',
+				},
+				files: {
+					'css/style-min.css':'sass/style.scss'
+				}
+			},
+		},
+
+		/* *
+		 * Watch Task
+		 */
+		watch: {
+			css: {
+				files: '**/*.scss',
+				tasks: ['sass']
+			}
+		} 
+
+
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.registerTask('default', ['watch']);
+}
